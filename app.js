047326f88d7dca5d87aa32b0a43334d19552200b
Diego <d.garciabrisa@gmail.com>
2021-02-20 12:35:21 +0100

    server port 3000 running

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const mongoose = require('mongoose')
 
-app.listen(port, `Listening at port localhost:${port}`)
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+
+
+app.use(router);
+
+app.listen(port, () => console.log(`Node server running on http://localhost:${port}`))
