669375a8553d6cb8c92b7154c22dd77015cf8b16
Diego <d.garciabrisa@gmail.com>
2021-03-25 16:17:56 +0100

    cors installed and cors problem fixed

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const router = require('./router');
 const db = require('./db');
+const cors = require('cors')
 const app = express();
 const port = 3000;
 
 
 // Middleware
 app.use(express.json())
+app.use(cors())
 app.use(router);
 
 
