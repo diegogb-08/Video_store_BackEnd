ca3082fced920c65dd56cdbf4da5af4c75aaf1a9
Diego <d.garciabrisa@gmail.com>
2021-04-03 17:01:39 +0200

    db.js amended to receive env from mongodb atlas

--- db.js
+++ db.js
@@ -1,15 +1,19 @@
 const mongoose = require('mongoose');
 
+const MONGO_SERVER = process.env.MONGO_SERVER || 'mongodb'
 const MONGO_HOST = process.env.MONGO_HOST || 'localhost';
 const MONGO_PORT = process.env.MONGO_PORT || '27017';
-const MONGO_DBNAME = process.env.MONGO_DBNAME || 'videostore';
+const MONGO_DBNAME = process.env.MONGO_DBNAME || 'fakeflix';
 const MONGO_USER = process.env.MONGO_USER || null;
 const MONGO_PASSWORD = process.env.MONGO_PASSWORD || null;
 
 const QUEARY_STRING = MONGO_USER ?
-`mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_HOST}:${MONGO_PORT}/${MONGO_DBNAME}`
+
+`${MONGO_SERVER}://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_HOST}/${MONGO_DBNAME}?retryWrites=true&w=majority`
+
 :
-`mongodb://${MONGO_HOST}:${MONGO_PORT}/${MONGO_DBNAME}`; 
+
+`mongodb://${MONGO_HOST}:${MONGO_PORT}/${MONGO_DBNAME}`
 
 // Connection to DB
 const db = mongoose.connect(QUEARY_STRING,{useNewUrlParser: true, useUnifiedTopology: true})
